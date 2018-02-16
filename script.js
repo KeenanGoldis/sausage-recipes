9f0d21c46ca71e4c0a45a121969403a96edd7faf
Keenan Goldis <keenangoldis@gmail.com>
2018-02-16 15:55:57 -0600

    Add Database

---
+++ script.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+var connection = mysql.createPool({
+  //properties...
+  connectionLimit: 50,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sausagerecipes'
+});
+
+//tempCont is used so that the server won't crash if too many people use it at the same time.
+//https://www.youtube.com/watch?v-hGZX_SA7IYg at 18 minutes for explanation..
+app.get('/', function (request, response){
+connection.getConnection(function(error, tempCont){
+  if(!!error) {
+    tempCont.release();
+    console.log('Error');
+  } else {
+    console.log('Connected!');
+    tempCont.query("SELECT * FROM allsausagerecipes", function(error, rows, fields){
+      tempCont.release();
+      if(!!error) {
+        console.log('Error in the query');
+      } else {
+        response.json(rows);
+      }
+    });
+    }
+  });
+});
+app.listen(1337);
